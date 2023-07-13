ab08401cfc0bf3119681d4566a3c47bea96c48f6
Tarek Maraoui <jumanga.contact@gmail.com>
2023-07-13 17:53:26 +0200

    entity ingredient(id,name,aisle) OK, entiry recipe(id, name, type, description + relations ) OK

---
+++ src/entities/recipe.entity.ts
@@ -0,0 +1,21 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToMany, JoinTable } from 'typeorm';
+import { Ingredient } from './ingredient.entity';
+
+@Entity()
+export class Recipe {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  type: string;
+
+  @Column()
+  description: string;
+
+  @ManyToMany(() => Ingredient)
+  @JoinTable()
+  ingredients: Ingredient[];
+}
